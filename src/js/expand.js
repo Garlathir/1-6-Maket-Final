d4a7b86018040ab84ff3a1dfabebae55797cbc66
Garlathir <izriviig@gmail.com>
2022-12-24 16:16:06 +0300

    Initial commit

---
+++ src/js/expand.js
@@ -0,0 +1,22 @@
+const expandArea = document.querySelectorAll('.expand-area')
+const expandBtn = document.querySelectorAll('.expand-btn')
+const expandBtnText = document.querySelectorAll('.expand-btn__text')
+const hide = 'Скрыть'
+
+const showMoreInfo = function (i, text) {
+   expandBtn[i].addEventListener('click', function () {
+      expandArea[i].classList.toggle('expand-btn-active')
+      expandBtn[i].classList.toggle('expand-btn--rotate')
+
+      const changeBtnText = function () {
+         if (expandBtnText[i].textContent === hide) {
+            return (expandBtnText[i].textContent = text)
+         } else if (expandBtnText[i].textContent === text) {
+            return (expandBtnText[i].textContent = hide)
+         }
+      }
+      changeBtnText(text)
+   })
+}
+showMoreInfo(1, 'Показать все')
+showMoreInfo(2, 'Показать все')
